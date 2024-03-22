7f665dd254c3765015b2f5b304505c7802e20977
lsp-infra <156368376+ls-infra@users.noreply.github.com>
2024-03-22 23:28:04 +1300

    add handleOnTextChanged

--- angluar-lang-client/src/app/app.component.ts
+++ angluar-lang-client/src/app/app.component.ts
@@ -3,10 +3,11 @@ import getKeybindingsServiceOverride from '@codingame/monaco-vscode-keybindings-
 // this is required syntax highlighting
 import '@codingame/monaco-vscode-groovy-default-extension';
 import { startEditor } from 'monaco-languageclient-examples';
-
-import { UserConfig } from 'monaco-editor-wrapper';
+import { IDisposable } from 'monaco-editor';
+import { UserConfig , MonacoEditorLanguageClientWrapper} from 'monaco-editor-wrapper';
 
 import { useWorkerFactory } from 'monaco-editor-wrapper/workerFactory';
+ 
 useWorkerFactory({
   rootPath: window.location.href + '../..',
   basePath: '../assets',
@@ -19,10 +20,30 @@ useWorkerFactory({
 })
 export class MonacoEditorComponent  implements AfterViewInit{
   title = 'angluar-lang-client';
-
+  
+  private _subscription: IDisposable | null = null;
   async ngAfterViewInit(): Promise<void> {
-
+ 
     runGroovyClient();
+}
+
+handleOnTextChanged(){
+  const  wrapper: MonacoEditorLanguageClientWrapper = new MonacoEditorLanguageClientWrapper();
+  const model = wrapper.getModel();
+  console.log('save click' , model)
+  if (model) {
+      const verifyModelContent = () => {
+          const modelText = model.getValue();
+       console.log(modelText)
+      };
+
+      this._subscription = model.onDidChangeContent(() => {
+          verifyModelContent();
+      });
+      console.log(this._subscription)
+      // do it initially
+      verifyModelContent();
+  }
 }
 }
 
